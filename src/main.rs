59bf5c34677f8a8830b83d488625a9d3b4d5ca99
Anton Sakhanovych <antonsakhanovych@gmail.com>
2023-11-19 23:17:17 +0100

    add message processing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::Arc;
 use std::thread;
@@ -6,12 +6,14 @@ use std::thread;
 fn main() {
     let address: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let listener = TcpListener::bind(address).unwrap();
+
     println!("Listenting on: {}", address);
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 let stream = Arc::new(stream);
-                thread::spawn(move || handleClient(stream));
+                thread::spawn(move || handle_client(stream));
                 ()
             }
             Err(e) => eprintln!("Couldn't accept because of {}", e),
@@ -19,8 +21,33 @@ fn main() {
     }
 }
 
-const message: &'static str = "Hello there stranger\n";
+const MESSAGE: &'static str = "Hello there stranger\n";
 
-fn handleClient(stream: Arc<TcpStream>) {
-    let _ = stream.as_ref().write(message.as_bytes());
+enum MessageError {
+    WrongSyntax,
+}
+
+fn handle_client(stream: Arc<TcpStream>) -> ! {
+    let mut stream = stream.as_ref();
+    let mut reader = BufReader::new(stream);
+    let _ = stream.write(MESSAGE.as_bytes());
+
+    loop {
+        let mut input = String::new();
+        let _ = reader.read_line(&mut input);
+        let response = match process_message(&input) {
+            Ok(message) => message,
+            Err(e) => match e {
+                MessageError::WrongSyntax => "Malformed Message\n".to_owned(),
+            },
+        };
+        let _ = stream.write(response.as_bytes());
+    }
+}
+
+fn process_message(message: &str) -> Result<String, MessageError> {
+    let mut splitted = message.split(' ');
+    let mut command = splitted.nth(0).unwrap().trim().to_owned();
+    command.push_str(" to you too!\n");
+    Ok(command)
 }
